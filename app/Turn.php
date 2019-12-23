d21c3bd673a1d93b0d37c8f55af7cfd542fe70bf
Howard <cliwfs_1994@icloud.com>
2019-12-23 08:37:38 -0500

    first

---
+++ app/Turn.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Turn extends Model
+{
+    public $incrementig = false;
+    public $fillable = ['player_id','location','type','game_id','id'];
+
+    public function game(){
+        $this->belongsTo('App\Game','game_id');
+    }
+    
+}
