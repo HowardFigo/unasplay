88e2027fc5f71a73d2a90f34ee76f4ef2fa0576e
Howard <cliwfs_1994@icloud.com>
2019-12-23 21:54:57 -0500

    last 1

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,8 +36,9 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => ' mt1',
-                'useTLS' => true,
+                'cluster' => 'eu',
+                 'encrypted' => false
+                
             ],
         ],
 
