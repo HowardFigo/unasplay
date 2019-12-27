736a492dea7515bf6648553c5763606f26ce6345
Howard <cliwfs_1994@icloud.com>
2019-12-26 21:17:58 -0500

    last

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -155,7 +155,7 @@
             else{
                 if(result == 'win'){
                     
-                    if($playerType == 'x'){
+                    if(data.type== "x"){
                     $('.profile-username').html(' ✨ SOLID allows to create an effective software, clean and flexible code and allows scalability. ✨ ');
                     }
                     else{
